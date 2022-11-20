27584d94ce2e56a3197ac4bbe6d42a1f8be7d7a0
LinoyYarkoni <linoyarkoni20@gmail.com>
2022-11-20 16:06:53 +0200

    game start update

--- A22 Ex05/RunGame.cs
+++ A22 Ex05/RunGame.cs
@@ -5,10 +5,7 @@
         public static void StartGame()
         {
             FormGame formGame = new FormGame();
-            FormScoreBoard fsb = new FormScoreBoard();
-            fsb.ShowDialog();
-            //FormGame formGame = new FormGame();
-            //formGame.ShowDialog();
+            formGame.ShowDialog();
         }
     }
 }
